630af0b823d687f370066ee3d82475ff1675be5c
Lui <luih@wit.edu>
2020-03-31 22:22:38 -0400

    the first baby steps to guinea pig mastery

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `GuineaPedia`,
+    description: `Cavies from beginners to experts!`,
+    author: `@halmonlui`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
