a512c098c2387a0c62f64d6d88659ce8cf8bb000
Lui <luih@wit.edu>
2020-03-31 22:50:00 -0400

    removed rellaxreact and used regular rellax. Set up index page so it is working with rellax as well as customize some image components and layout.

--- src/components/image.js
+++ src/components/image.js
@@ -16,7 +16,7 @@ import Img from "gatsby-image"
 const Image = () => {
   const data = useStaticQuery(graphql`
     query {
-      placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
+      placeholderImage: file(relativePath: { eq: "one_lettuce_pig.png" }) {
         childImageSharp {
           fluid(maxWidth: 300) {
             ...GatsbyImageSharpFluid
