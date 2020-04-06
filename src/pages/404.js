9cecb6f598025586dca2fc1affeb157b95e27feb
Lui <luih@wit.edu>
2020-04-05 21:16:00 -0400

    add 404 page not found page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,18 @@
 import React from "react"
 
+import Image from "../components/image"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <section className="section-404">
+      <div className="content">
+        <h1>NOT FOUND</h1>
+        <p>The page you're looking for doesn't exist :)</p>
+      </div>
+    </section>
   </Layout>
 )
 
